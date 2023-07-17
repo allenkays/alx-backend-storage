7b8f91e017cd3bde3b556a44fdce4f985d73d7e1
allenkays <allenkays@tutanota.com>
2023-07-17 22:23:41 +0300

    Add script to create table if not exist with the given requirements

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -2,7 +2,7 @@
 -- With these attributes:
 -- id, integer, never null, auto increment and primary key email, string (255 characters), never null and unique name, string (255 characters)
 CREATE TABLE IF NOT EXISTS users (
-  id INTERGER AUTO_INCREMENT PRIMARY KEY,
+  id INTERGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
   email VARCHAR(255) NOT NULL UNIQUE,
   name VARCHAR(255)
   );
