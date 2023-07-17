a42a99e615a1f6012f31e726c400081412ed7a28
allenkays <allenkays@tutanota.com>
2023-07-17 23:56:35 +0300

    Add split year for select statement

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,6 @@
 -- SQL script that lists all bands with Glam rock as their main style, ranked by their longevity
-SELECT band_name, (YEAR("2022-01-01")- CAST(SUBSTRING_INDEX(lifespan, '-', 1) AS UNSIGNED)) AS lifespan
+SELECT DISTINCT band_name,
+IF NULL(split 2022) - formed AS lifespan
 FROM metal_bands
 WHERE main_style = 'Glam rock'
-ORDER BY lifespan DESC;)
+ORDER BY lifespan DESC;
