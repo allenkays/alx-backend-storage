d2ad2c3098965b2ba8fa879810a7f34f6847be2c
allenkays <allenkays@tutanota.com>
2023-07-17 23:36:25 +0300

    Update statement to include DISTINCT

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,7 +1,7 @@
 -- SQL script that ranks country origins of bands
 -- Column names must be: origin and nb_fans
 
-SELECT origin, COUNT(*) AS nb_fans
+SELECT DISTINCT origin, COUNT(*) AS nb_fans
 FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
