6c0f30a4631bcaec9f73638675412053a6686c30
allenkays <allenkays@tutanota.com>
2023-07-17 22:55:20 +0300

    Add back ticks to params

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,12 +1,9 @@
 -- SQL script that creates a table users following these requirements:
--- With these attributes:
--- id, integer, never null, auto increment and primary key email, string (255 characters), never null and unique name, string (255 characters)
-
+-- attributes:id, email, name
 -- Creates a table users only if it does not exist
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` INT AUTO_INCREMENT NOT NULL,
-  `email` VARCHAR(255) NOT NULL,
-  `name` VARCHAR(255)
-  PRIMARY KEY (id),
-  UNIQUE (email)
-  )ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS `users`(
+`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+`email` VARCHAR(255) NOT NULL UNIQUE,
+`name` VARCHAR(255)
+);
