9509c16aac7a591b60f7c22cdfe09d789b987665
allenkays <allenkays@tutanota.com>
2023-07-17 23:09:29 +0300

    Remove back ticks

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -2,10 +2,10 @@
 -- attributes:id, email, name
 -- Creates a table users only if it does not exist
 
-CREATE TABLE IF NOT EXISTS `users`(
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `email` VARCHAR(255) NOT NULL,
-  `name` VARCHAR(255),
+CREATE TABLE IF NOT EXISTS users(
+  id INT NOT NULL AUTO_INCREMENT,
+  email VARCHAR(255) NOT NULL,
+  name VARCHAR(255),
   PRIMARY KEY (id),
   UNIQUE (email)
 );
