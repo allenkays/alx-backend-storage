33b5a853b438dac37a39ccc65670c0be3bf1195c
allenkays <allenkays@tutanota.com>
2023-07-17 22:52:17 +0300

    Add engine to MySQL script

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -3,10 +3,10 @@
 -- id, integer, never null, auto increment and primary key email, string (255 characters), never null and unique name, string (255 characters)
 
 -- Creates a table users only if it does not exist
-CREATE TABLE IF NOT EXISTS users (
-  id INT AUTO_INCREMENT NOT NULL,
-  email VARCHAR(255) NOT NULL,
-  name VARCHAR(255)
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` INT AUTO_INCREMENT NOT NULL,
+  `email` VARCHAR(255) NOT NULL,
+  `name` VARCHAR(255)
   PRIMARY KEY (id),
   UNIQUE (email)
   )ENGINE=InnoDB;
