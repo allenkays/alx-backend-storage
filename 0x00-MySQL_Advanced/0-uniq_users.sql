05a173f6f262f19c1401ef372a07c774d2f3591d
allenkays <allenkays@tutanota.com>
2023-07-17 23:03:28 +0300

    Remove duplicate declaration of unique

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -3,9 +3,7 @@
 -- Creates a table users only if it does not exist
 
 CREATE TABLE IF NOT EXISTS `users`(
-  `id` INT NOT NULL AUTO_INCREMENT,
+  `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `email` VARCHAR(255) NOT NULL UNIQUE,
   `name` VARCHAR(255),
-  PRIMARY KEY (id),
-  UNIQUE (email)
 );
