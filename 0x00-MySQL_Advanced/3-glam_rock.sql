2c47a6b365fe7a6d1f24c454b37c975be21226a3
allenkays <allenkays@tutanota.com>
2023-07-18 00:01:44 +0300

    Add style to WHERE statement

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
 -- SQL script that lists all bands with Glam rock as their main style, ranked by their longevity
 SELECT DISTINCT band_name,
-IF NULL(split 2022) - formed AS lifespan
+IFNULL(`split`, 2022) - formed AS lifespan
 FROM metal_bands
-WHERE main_style = 'Glam rock'
+WHERE FIND_IN_SET('Glam rock', style)
 ORDER BY lifespan DESC;
