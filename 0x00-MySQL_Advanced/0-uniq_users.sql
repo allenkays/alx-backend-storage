184385ba5f650aece9ec64afef85757ab981c5fb
allenkays <allenkays@tutanota.com>
2023-07-17 23:00:22 +0300

    Put primary key and unique at end of script

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -3,7 +3,9 @@
 -- Creates a table users only if it does not exist
 
 CREATE TABLE IF NOT EXISTS `users`(
-`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-`email` VARCHAR(255) NOT NULL UNIQUE,
-`name` VARCHAR(255)
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `email` VARCHAR(255) NOT NULL UNIQUE,
+  `name` VARCHAR(255)
+  PRIMARY KEY (id),
+  UNIQUE (email)
 );
