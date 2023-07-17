dd520b7c97cff12ce1707d967bb386729105839c
allenkays <allenkays@tutanota.com>
2023-07-17 23:01:33 +0300

    Add comma at end of variable declaration

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -5,7 +5,7 @@
 CREATE TABLE IF NOT EXISTS `users`(
   `id` INT NOT NULL AUTO_INCREMENT,
   `email` VARCHAR(255) NOT NULL UNIQUE,
-  `name` VARCHAR(255)
+  `name` VARCHAR(255),
   PRIMARY KEY (id),
   UNIQUE (email)
 );
