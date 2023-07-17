67f629792ac394591f90a936726f9759467725dc
allenkays <allenkays@tutanota.com>
2023-07-17 22:29:41 +0300

    Separate primary key and unique statements from the rest of the code

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,8 +1,12 @@
 -- SQL script that creates a table users following these requirements:
 -- With these attributes:
 -- id, integer, never null, auto increment and primary key email, string (255 characters), never null and unique name, string (255 characters)
+
+-- Creates a table users only if it does not exist
 CREATE TABLE IF NOT EXISTS users (
-  id INTERGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
-  email VARCHAR(255) NOT NULL UNIQUE,
+  id INT AUTO_INCREMENT NOT NULL,
+  email VARCHAR(255) NOT NULL,
   name VARCHAR(255)
+  PRIMARY KEY (id),
+  UNIQUE (email)
   );
