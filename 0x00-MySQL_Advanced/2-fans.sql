763404d213fc2a9be4fda5eeeaec5240a70328c0
allenkays <allenkays@tutanota.com>
2023-07-17 23:39:35 +0300

    Remove * selectore for all

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,7 +1,7 @@
 -- SQL script that ranks country origins of bands
 -- Column names must be: origin and nb_fans
 
-SELECT DISTINCT origin, SUM(*) AS nb_fans
+SELECT DISTINCT origin, SUM(fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
