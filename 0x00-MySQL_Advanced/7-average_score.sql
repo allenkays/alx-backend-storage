110b71dbc461e8df1619136898fa33f48965cccc
allenkays <allenkays@tutanota.com>
2023-08-09 14:27:16 +0300

    Update stored procedure to calculate score count based on number of scores of a given user and not projects

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,26 +1,10 @@
--- SQL script that creates a stored procedure ComputeAverageScoreForUser
-DELIMITER $$
-CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+-- SQL script that creates a stored procedure ComputeAverageScoreForUser that computes and store the average score for a student. 
+DELIMITER |
+CREATE PROCEDURE ComputeAverageScoreForUser (
+    IN user_id INT)
 BEGIN
-  DECLARE total_score FLOAT;
-  DECLARE total_projects INT;
-  DECLARE avg_score FLOAT;
-
-  /* Calculate total score for the user */
-  SELECT SUM(CAST(score AS FLOAT)) INTO total_score FROM corrections WHERE user_id = user_id;
-
-  /* Count the number of projects for the user */
-  SELECT COUNT(*) INTO total_projects FROM corrections WHERE user_id = user_id;
-
-  /* Calculate average score */
-  IF total_projects > 0 THEN
-    SET avg_score = total_score / total_projects;
-  ELSE
-    SET avg_score = 0;
-  END IF;
-
-  /* Update the user's average score */
-  UPDATE users SET average_score = avg_score WHERE id = user_id;
+    DECLARE avg_score FLOAT;
+    SET avg_score = (SELECT SUM(score) / COUNT(score) FROM corrections WHERE corrections.user_id = user_id);
+    UPDATE users SET average_score = avg_score WHERE id = user_id;
 END;
-$$
-DELIMITER ;
+|
