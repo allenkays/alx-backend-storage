a7abdad9394b2b3844e09e34e49023613112a04a
allenkays <allenkays@tutanota.com>
2023-08-09 19:48:20 +0300

    Add MySQL function for interger division

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,12 +1,12 @@
 -- SQL script that creates a function SafeDiv that divides (and returns) the first by the second number or returns 0 if the second number is equal to 0.
 DELIMITER $$
-CREATE FUNCTION SafeDiv(IN a INT, IN b INT) OUT FLOAT
+CREATE FUNCTION SafeDiv(a INT, b INT) RETURNS FLOAT
 BEGIN
   IF b > 0 THEN
     RETURN a / b;
   ELSE
     RETURN 0;
-  END IF
+  END IF;
 END;
 $$
 DELIMITER ;
