6dcad878eafd4268954831a2dd7fff95aa7dd6cf
allenkays <allenkays@tutanota.com>
2023-10-16 09:57:52 +0300

    Add python script to find list of school by topic

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+"""
+import pymongo
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    Returns list of school having a specific topic
+
+    :param mongo_collection: pymongo collection object
+    :topic: Topic (string) to search for
+    :returns: List of school with given topic
+    """
+    result = mongo_collection.find({"topics": topic})
+    results = list(result)
+
+    return results
