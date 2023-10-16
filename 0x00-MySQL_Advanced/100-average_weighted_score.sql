3c7bd0b42f164b600096ab588ae294989e2ef1bb
allenkays <allenkays@tutanota.com>
2023-10-16 10:39:31 +0300

    Add stored procedure to calculate total weighted score

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -1,14 +1,34 @@
 --  SQL script that creates a stored procedure ComputeAverageWeightedScoreForUser that computes and store the average weighted score for a student.
-DELIMETER $$
-CREATE PROCEDURE ComputeAverageWeightedScoreForUser(user_id INT)
+DELIMITER //
+
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
 BEGIN
-  DECLARE w_avg_score FLOAT;
-  SET w_avg_score = (SELECT SUM(score * weight) / SUM(weight)
-    FROM users AS U 
-    JOIN corrections as C ON U.id=C.user_id 
-    JOIN projects AS P ON C.project_id=P.id 
-    WHERE U.id=user_id);
-  UPDATE users SET average_score = w_avg_score WHERE id=user_id;
-END $$
+    -- Declare variables to hold the computed values
+    DECLARE total_weighted_score DECIMAL(10, 2);
+    DECLARE total_weight DECIMAL(10, 2);
+    DECLARE average_weighted_score DECIMAL(10, 2);
+
+    -- Calculate the total weighted score and total weight for the user
+    SELECT
+        SUM(c.score * p.weight) INTO total_weighted_score,
+        SUM(p.weight) INTO total_weight
+    FROM
+        corrections c
+    JOIN
+        projects p ON c.project_id = p.id
+    WHERE
+        c.user_id = user_id;
+
+    -- Check if there are scores for the user
+    IF total_weight IS NOT NULL THEN
+        -- Calculate the average weighted score
+        SET average_weighted_score = total_weighted_score / total_weight;
+
+        -- Update the user's average_score in the users table
+        UPDATE users
+        SET average_score = average_weighted_score
+        WHERE id = user_id;
+    END IF;
+END //
 
 DELIMITER ;
